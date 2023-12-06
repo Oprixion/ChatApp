667c7fec8b0a3590ca7059e5b30a45eff0913241
mendozajm198 <mendozajm198@yahoo.com>
2023-12-05 18:06:12 -0800

    added collapsible sidebar

--- js/script.js
+++ js/script.js
@@ -130,10 +130,14 @@ function displayMessage(username, message, user) {
   
 }
 
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "20%";
+  document.getElementById("main").style.marginLeft = "20%";
+}
 
-
-
-
-
-
-
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("chat").style.marginLeft = "0";
+}
